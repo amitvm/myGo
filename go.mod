2d8351d27e094b1a7140bbf077871f8c1608098c
Al ameen <alameen.info@gmail.com>
2024-03-26 06:30:58 +0530

    Updating encrypt module V2.0.0

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/amitvm/myGo
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/amitvm/myGo/encrypt"
+)
+
+func main() {
+	fmt.Println(encrypt.Nimbus("test"))
+}
